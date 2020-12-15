954d7b48067207272c8600157deeb3bc0d76b3c7
Mateusz Matusiak <mateusz.matusiak@ardentcode.com>
2020-12-15 20:29:50 +0100

    Updates

---
+++ precache-manifest.e3007b6ecfd948d28fa53d2bf1a194ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "259504d8555cf54996cdb2466f66f8b2",
+    "url": "/kryptokombajn/index.html"
+  },
+  {
+    "revision": "3b041e2a55f64a53767d",
+    "url": "/kryptokombajn/static/css/main.117e9a2f.chunk.css"
+  },
+  {
+    "revision": "355dfdce212fc5ca3ffd",
+    "url": "/kryptokombajn/static/js/2.64bce4ed.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/kryptokombajn/static/js/2.64bce4ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b041e2a55f64a53767d",
+    "url": "/kryptokombajn/static/js/main.27d70f5c.chunk.js"
+  },
+  {
+    "revision": "a5fb79325a1e5165e067",
+    "url": "/kryptokombajn/static/js/runtime-main.af34a12f.js"
+  }
+]);
